ef1b7d40879d0a6e02ce84bf64636b2c65f13d72
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-08-09 17:23:11 +0300

    Added an async action creator

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,10 @@
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+// Async Action creator
 export const fetchPosts = () => {
-  return {
-    type: "FETCH_POSTS",
+  return async (dispatch) => {
+    const response = await jsonPlaceholder.get("/posts");
+
+    dispatch({ type: "FETCH_POSTS", payload: response });
   };
 };
