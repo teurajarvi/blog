2d48e32550e589b202ede42fb5bd6aa1d3d4ff53
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-08-10 14:02:45 +0300

    Refactored UserHeader mapStateToProps to find the user intead of the components render method

--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -6,17 +6,20 @@ class UserHeader extends React.Component {
   componentDidMount() {
     this.props.fetchUser(this.props.userId);
   }
+
   render() {
-    const user = this.props.users.find((user) => user.id === this.props.userId);
+    const { user } = this.props;
     if (!user) {
       return null;
     }
+
     return <div className='header'>{user.name}</div>;
   }
 }
 
-const mapStateToProps = (state) => {
-  return { users: state.users };
+// used ownProps to coming into our component
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find((user) => user.id === ownProps.userId) };
 };
 
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
